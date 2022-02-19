3b01530165eac8a7f4b73b0b59dc4558809d6876
joão gabriel <joaoassisgabriel@gmail.com>
2022-02-19 13:10:34 -0300

    João Gabriel -J

---
+++ SistemaAcademico/SistemaAcademico/Model/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Model
+{
+    class Pessoa
+    {
+        //Atributos - Variáveis de Classe
+        public long cpf;
+        public string nome;
+        public DateTime dataNascimento;
+    }
+}
